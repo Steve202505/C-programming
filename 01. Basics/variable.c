95ba48c2a684a48d66e5229d39ce4e50a7613713
Steve202505 <oceantaste12@gmail.com>
2025-06-14 16:36:06 +0000

    commit

--- 01. Basics/variable.c
+++ 01. Basics/variable.c
@@ -2,5 +2,11 @@
 int main()
 {
     char name[30] = "Hitesh";
-    printf(name);
+    int age = 35;
+    float grade = 3.55;
+    double hieght = 5.44;
+    printf("My name is %s\n", name);
+    printf("My age is %d\n", age);
+    printf("My BSC CGPA is %.2f\n", grade);
+    printf("My Hieght is %lf\n", hieght);
 }
